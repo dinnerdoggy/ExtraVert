59bfc8fd02bf39d89c5c77c4d7be2b21b5296fe8
Casey <dinnerdoggy@gmail.com>
2025-01-06 21:33:16 -0600

    declaring a class and accessing it

--- Plant.cs
+++ Plant.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\n\t\tHello, World! It's me! Casey!\n");
+﻿using System;
+
+public class Plant
+{
+   public string Species = "Basil";
+   public int LightNeeds = 5;
+   public decimal AskingPrice = 5.00m;
+   public string City = "Nashville";
+   public int Zip = 37214;
+   public bool Sold = false;
+};
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of the Plant class
+        Plant plant = new Plant();
+
+        // Access the Species property of the instance
+        Console.WriteLine($"Species: {plant.Species}");
+    }
+}
