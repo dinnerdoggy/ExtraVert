bd158875fce5c4aa91a5f8a8661ef5fd4196de79
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2025-01-07 14:18:45 -0600

    .

--- Plant.cs
+++ Plant.cs
@@ -2,22 +2,36 @@
 
 public class Plant
 {
-   public string Species = "Basil";
-   public int LightNeeds = 5;
-   public decimal AskingPrice = 5.00m;
-   public string City = "Nashville";
-   public int Zip = 37214;
-   public bool Sold = false;
+   public string Species;
+   public int LightNeeds;
+   public decimal AskingPrice;
+   public string City;
+   public int Zip;
+   public bool Sold;
+
+   public Plant(string species, int lightneeds, decimal askingprice, string city, int zip, bool sold)
+    {
+        Species = species;
+        LightNeeds = lightneeds;
+        AskingPrice = askingprice;
+        City = city;
+        Zip = zip;
+        Sold = sold;
+    }
 };
 
 class Program
 {
-    static void Main()
-    {
-        // Create an instance of the Plant class
-        Plant plant = new Plant();
-
-        // Access the Species property of the instance
-        Console.WriteLine($"Species: {plant.Species}");
+    static void Main(string[] args)
+     {
+        Plant plant1 = new Plant(
+            "Basil",
+            5,
+            5.00m,
+            "Nashville",
+            37214,
+            true
+        );
+    Console.WriteLine( plant1.Species );
     }
 }
